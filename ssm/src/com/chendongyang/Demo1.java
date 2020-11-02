830e8242771745ba97b84764064c9c0a24be7206
8840 <416101162@qq.com>
2020-11-02 19:42:08 +0800

    组长提交

--- ssm/src/com/chendongyang/Demo1.java
+++ ssm/src/com/chendongyang/Demo1.java
@@ -2,6 +2,6 @@ package com.chendongyang;
 
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("idea git demo");
+        System.out.println("idea git demo2222");
     }
 }
